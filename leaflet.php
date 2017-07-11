c5502a9a1426253d40de0323b3254afedcd68a92
Juh <nibreh@users.noreply.github.com>
2017-07-11 12:02:41 +0200

    Update Yellow 0.7.1

--- leaflet.php
+++ leaflet.php
@@ -5,20 +5,24 @@
 // Leaflet plugin by nibreh - http://leafletjs.com/
 class YellowLeaflet
 {
-	const VERSION = "0.6.9";
+	const VERSION = "0.7.1";
 	var $yellow;			//access to API
 
 	// Handle initialisation
 	function onLoad($yellow)
 	{
 		$this->yellow = $yellow;
-		$this->yellow->config->setDefault("LeafletJs", "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.3/leaflet.js");
-		$this->yellow->config->setDefault("LeafletCss", "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.3/leaflet.css");
-		$this->yellow->config->setDefault("ClusterJs", "https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.0.3/leaflet.markercluster.js");
-		$this->yellow->config->setDefault("ClusterCss", "https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.0.3/MarkerCluster.css");
-		$this->yellow->config->setDefault("ClusterCssDefault", "https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.0.3/MarkerCluster.Default.css");
+		$this->yellow->config->setDefault("LeafletJs", "https://unpkg.com/leaflet@1.1.0/dist/leaflet.js");
+		$this->yellow->config->setDefault("LeafletCss", "https://unpkg.com/leaflet@1.1.0/dist/leaflet.css");
+		$this->yellow->config->setDefault("ClusterJs", "https://unpkg.com/leaflet.markercluster@1.0.6/dist/leaflet.markercluster.js");
+		$this->yellow->config->setDefault("ClusterCss", "https://unpkg.com/leaflet.markercluster@1.0.6/dist/MarkerCluster.css");
+		$this->yellow->config->setDefault("ClusterCssDefault", "https://unpkg.com/leaflet.markercluster@1.0.6/dist/MarkerCluster.Default.css");
+		$this->yellow->config->setDefault("FullscreenCss", "https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/leaflet.fullscreen.css");
+		$this->yellow->config->setDefault("FullscreenJs", "https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/Leaflet.fullscreen.min.js");
 		$this->yellow->config->setDefault("LeafletLongitude", "48.000");
 		$this->yellow->config->setDefault("LeafletLatitude", "2.000");
+		$this->yellow->config->setDefault("LeafletHeight", "500px");
+		$this->yellow->config->setDefault("LeafletZoom", "5");
 	}
 
 	// Handle page content parsing of custom block
@@ -30,20 +34,22 @@ class YellowLeaflet
 			list($longitude, $latitude, $height, $zoom ) = $this->yellow->toolbox->getTextArgs($text);
 			if(empty($longitude)) $longitude = $this->yellow->config->get("LeafletLongitude");
 			if(empty($latitude)) $latitude = $this->yellow->config->get("LeafletLatitude");
-			if(empty($height)) $height = "500px";
-			if(empty($zoom)) $zoom = "5";
-			$output = "<div id=\"leaflet\" style=\"height:".htmlspecialchars($height)."; z-index:0;\">\n";
-			$output .="</div>\n";
-			$output .= "<script type=\"text/javascript\">\n";
-			$output .= "var map = L.map('leaflet', {";
-			$output .= "center: [".strencode($longitude).", ".strencode($latitude)."], ";
-			$output .= "zoom: ".strencode($zoom)."";
-			$output .= "});\n";
-			$output .= "L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {";
-			$output .= "attribution: '&copy; <a href=\"https://www.openstreetmap.org\">OpenStreetMap</a>',";
-			$output .= "}).addTo(map);\n";
-			$output .= "var markers = L.markerClusterGroup();\n";
-			$output .= "map.addLayer(markers);\n";
+			if(empty($height)) $height = $this->yellow->config->get("LeafletHeight");
+			if(empty($zoom)) $zoom = $this->yellow->config->get("LeafletZoom");
+			$output = "<div id=\"leaflet\" style=\"height:".htmlspecialchars($height)."; z-index:0;\"></div>\n";
+			$output .= "<script type=\"text/javascript\"> \n";
+			// TILE LAYER
+			$output .= "var tileLayer = new L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {";
+        	$output .= "attribution: '&copy; <a href=\"https://www.openstreetmap.org\">OpenStreetMap</a>'}); \n";	
+			// CLUSTERGROUP AND SUBGROUP
+			$output .= "var markers = new L.markerClusterGroup(); \n";
+			// VAR MAP
+			$output .= "var map = L.map('leaflet', {\n";
+			$output .= "center: [".strencode($longitude).", ".strencode($latitude)."], \n";
+			$output .= "zoom: ".strencode($zoom).", \n";
+			$output .= "layers: [tileLayer, markers]}); \n";
+			// FULLSCREEN
+			$output .= "map.addControl(new L.Control.Fullscreen());\n";
 			$output .= "</script>\n";
 		}
 
@@ -57,7 +63,7 @@ class YellowLeaflet
 			if(!empty($adress)) $output .= htmlspecialchars($adress)."<br />";
 			if(!empty($textlink)) $output .= "<a href='".htmlspecialchars($url)."'>".htmlspecialchars($textlink)."</a>";
 			$output .= "\").closePopup();\n";
-			$output .= "markers.addLayer(marker);\n";
+			$output .= "marker.addTo(markers);\n";
 			$output .= "</script>\n";
 		}
 		return $output;
@@ -74,11 +80,15 @@ class YellowLeaflet
 			$ClusterJs = $this->yellow->config->get("ClusterJs");
 			$ClusterCss = $this->yellow->config->get("ClusterCss");
 			$ClusterCssDefault = $this->yellow->config->get("ClusterCssDefault");
-			$output .= "<script type=\"text/javascript\" src=\"{$LeafletJs}\"></script>\n";
-			$output .= "<script type=\"text/javascript\" src=\"{$ClusterJs}\"></script>\n";
+			$FullscreenCss = $this->yellow->config->get("FullscreenCss");
+			$FullscreenJs = $this->yellow->config->get("FullscreenJs");
 			$output .= "<link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"{$LeafletCss}\">\n";
 			$output .= "<link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"{$ClusterCss}\">\n";
 			$output .= "<link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"{$ClusterCssDefault}\">\n";
+			$output .= "<link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"{$FullscreenCss}\">\n";
+			$output .= "<script type=\"text/javascript\" src=\"{$LeafletJs}\"></script>\n";
+			$output .= "<script type=\"text/javascript\" src=\"{$ClusterJs}\"></script>\n";
+			$output .= "<script type=\"text/javascript\" src=\"{$FullscreenJs}\"></script>\n";
 		}
 		return $output;
 	}
